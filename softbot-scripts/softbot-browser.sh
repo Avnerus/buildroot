bdb3d6227fc683556ccea43291d1ffe10be66659
avnerus <avnerus@gmail.com>
2019-06-29 20:29:28 +0300

    overlay with nginx config. Optimize streaming to threads = 1 because of chrome freezes

--- softbot-scripts/softbot-browser.sh
+++ softbot-scripts/softbot-browser.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export WEBKIT_INSPECTOR_SERVER=0.0.0.0:1234
 export WPE_BCMRPI_CURSOR=1
-cog --set-permissions=all --enable-webaudio=true --enable-mediasource=true --enable-media-stream=true --media-playback-allows-inline=true --enable-media-capabilities=true https://softbot.avner.us/avatar
+cog --set-permissions=all --enable-webaudio=true --enable-mediasource=true --enable-media-stream=true --media-playback-allows-inline=true --enable-media-capabilities=true https://hitodama.online/avatar
